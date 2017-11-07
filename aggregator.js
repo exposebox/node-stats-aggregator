776d9d803c9875adcd0f1d52c04c75869623b9fa
Orel <orel@exposebox.com>
2017-11-07 10:02:01 +0200

    stupid errors, debug printing

--- aggregator.js
+++ aggregator.js
@@ -29,7 +29,7 @@ class StatsAggregator {
     }
 
     getOrCreateStat(keyValues) {
-        const dataMapKey = _.keys(keyValues).join('.');
+        const dataMapKey = _.map(this.keyFields, kf => keyValues[kf]).join('.');
         const dataForKey = this.data[dataMapKey];
         if (dataForKey)
             return dataForKey;
